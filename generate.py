d445b73e00bd49cfd61cc6033f6ac66a949b19cb
CB <2475167+terbo@users.noreply.github.com>
2023-01-18 19:35:25 -0600

    add csv, yaml, etc
    default settings, yaml configuration and switching, prompts from text, or csv with model switching,
    configurable output directory, way too many todos

--- generate.py
+++ generate.py
@@ -1,56 +1,114 @@
-# Using Automatic 1111 WebUI, webuiapi and PIL,
-# generate an image from the command line
+"""
+Using Automatic 1111 WebUI, webuiapi and PIL,
+generate an image from the command line
 
-# TODO: csv file reading, config file
-#       img2img, loopback, scripts, hires fix
+CSV format:
+prompt,negative_prompt,width,height,steps,seed,cfg,sampler,model,style
 
-# BUGS: If the server crashes (or in my case the ssh connection times out)
-#       the API blocks.
+Enclose strings in qoutes.
+
+Requires https://github.com/AUTOMATIC1111/stable-diffusion-webui with '--api' command line flag
+Based on https://github.com/mix1009/sdwebuiapi
+
+TODO: tiling, img2img, loopback, scripts, hires fix, upscaling, face restore
+      if making a batch, create a collage of all images, maybe
+      generate more unique output files
+
+BUGS: If the server crashes (or in my case the ssh tunnel times out..)
+      the API blocks.
+"""
 
 import argparse, string, random
 import re, sys
 import webuiapi
 from PIL import Image
 
-seed_base = 1e15 # 1000000000000000
+defaults = {}
+
+def read_yaml(yaml_filename='generate.yaml'):
+  if len(sys.argv) > 3 and sys.argv[1] == '--yaml':
+    yaml_filename = sys.argv[2]
+  
+  try:
+    import yaml
+    
+    with open(yaml_filename, "r") as ymlfile:
+      yamlcfg = yaml.safe_load(ymlfile)
+      
+      defaults['width'] = yamlcfg['width']
+      defaults['height'] = yamlcfg['height']
+      defaults['negative_prompt'] = yamlcfg['negative_prompt']
+      defaults['steps'] = yamlcfg['steps']
+      defaults['seed'] = yamlcfg['seed']
+      defaults['cfg'] = yamlcfg['cfg']
+      defaults['sampler'] = yamlcfg['sampler']
+      defaults['model'] = yamlcfg['model']
+      defaults['batch'] = yamlcfg['batch']
+      defaults['styles'] = yamlcfg['styles']
+      defaults['host'] = yamlcfg['host']
+      defaults['port'] = yamlcfg['port']
+      defaults['output_dir'] = yamlcfg['output_dir']
+      defaults['seed_base'] = yamlcfg['seed_base']
+
+  except Exception as e:
+    print('Loading yaml: ', e, sys.argv)
+    
+    defaults['width'] = 512
+    defaults['height'] = 512
+    defaults['negative_prompt'] = ''
+    defaults['steps'] = 20
+    defaults['seed'] = 0
+    defaults['cfg'] = 7
+    defaults['sampler'] = 'DPM++ 2S a Karras'
+    defaults['model'] = 'v1-5-pruned-emaonly.ckpt'
+    defaults['batch'] = 1
+    defaults['styles'] = ''
+    defaults['host'] = 'localhost'
+    defaults['port'] = 7860
+    defaults['output_dir'] = 'images'
+    defaults['seed_base'] = 1e15  # 1000000000000000
 
 def parse_args():
     parser=argparse.ArgumentParser(prog='generate',
                                    description='Partial command line interface for Automatic 1111 Web UI',
                                    epilog='http://github.com/terbo/sdscripts')
-    parser.add_argument('--prompt', help='prompt to generate image from', default='happy little trees, style of bob ross')
+    
+    parser.add_argument('--prompt', help='prompt to generate image from')
     parser.add_argument('--prompts', help='read prompts from a text file')
-#   parser.add_argument('--promptcsv', help='read prompts and generation options from a csv file, format incoming')
+    parser.add_argument('--promptcsv', help='read prompts and generation options from a csv file')
 
-    parser.add_argument('--negative', help='negative prompt', default='')
-    parser.add_argument('--steps', help='sampling steps', default=20)
-    parser.add_argument('--seed', help='default seed', default=0)
-    parser.add_argument('--cfg', help='CFG scale (0-30)', default=7)
-    parser.add_argument('--sampler', help='sampler to use', default='DPM++ 2S a Karras')
-    parser.add_argument('--style', help='saved style to apply, use --liststyles to view styles', default='')
+    parser.add_argument('--width', help='Image width', default=defaults['width'])
+    parser.add_argument('--height', help='Image height', default=defaults['height'])
+    
+    parser.add_argument('--negative_prompt', help='negative prompt', default=defaults['negative_prompt'])
+    parser.add_argument('--steps', help='sampling steps', default=defaults['steps'])
+    parser.add_argument('--seed', help='default seed', default=defaults['seed'])
+    parser.add_argument('--cfg', help='CFG scale (0-30)', default=defaults['cfg'])
+    parser.add_argument('--sampler', help='sampler to use', default=defaults['sampler'])
+    parser.add_argument('--styles', help='comma seperated list of styles to apply, use --liststyles to view styles', default=defaults['styles'])
 
-    parser.add_argument('--batch', help='number of images to create', default=1)
+    parser.add_argument('--batch', help='number of images to create', default=defaults['batch'])
+    parser.add_argument('--output_dir', help='directory to output images to', default=defaults['output_dir'])
     
-    parser.add_argument('--host', help='WebUI API Host', default='omni')
-    parser.add_argument('--port', help='WebUI API Port', default='7860')
+    parser.add_argument('--host', help='WebUI API Host', default=defaults['host'])
+    parser.add_argument('--port', help='WebUI API Port', default=defaults['port'])
     
-    parser.add_argument('--width', help='Image width', default=512)
-    parser.add_argument('--height', help='Image height', default=512)
-    
+    parser.add_argument('--model', help='set default model in UI')
     parser.add_argument('--getmodel', action='store_true', help='show default model in UI')
-    parser.add_argument('--setmodel', help='set default model in UI')
     parser.add_argument('--listmodelsfull', action='store_true', help='list available models with all information')
     parser.add_argument('--listmodels', action='store_true', help='list available models (2)')
     
     parser.add_argument('--listsamplers', action='store_true', help='list available samplers')
     parser.add_argument('--liststyles', action='store_true', help='list available prompt styles')
    
+    parser.add_argument('--yaml', help='select yaml configuration', default='generate.yaml')
     parser.add_argument('--getoptions', action='store_true', help='return current options')
     parser.add_argument('--showimg', action='store_true', help='show generated image')
+    parser.add_argument('--saveinfo', action='store_true', help='save generation information')
     parser.add_argument('--showinfo', action='store_true', help='show generation information')
     
     opts=parser.parse_args()
-    return opts
+    return (opts, parser.print_help)
 
 # from automatic1111 modules\images.py
 def sanitize_filename_part(text, replace_spaces=True):
@@ -73,66 +131,34 @@ def sanitize_filename_part(text, replace_spaces=True):
   text = text.rstrip(invalid_filename_postfix)
   return text
 
-def generate_image(opts, negative_prompt='(bad_prompt_version2:0.8)'):
-  if not opts.seed:
-    seed = int(random.random() * 1e10)
-
-  config = {
-          'prompt': opts.prompt,
-          'width': opts.width,
-          'height': opts.height,
-          'negative_prompt': negative_prompt,
-          'cfg': opts.cfg,
-          'sampler': opts.sampler,
-          'steps': opts.steps,
-          'seed': seed,
-  }
+def generate_image(options):
+  if (not options.seed) or (options.seed == '0') or (options.seed == 0):
+    options.seed = int(random.random() * 1e10)
   
-  res = api.txt2img(prompt=config['prompt'],
-                    negative_prompt=config['negative_prompt'],
-                    width=config['width'],
-                    height=config['height'],
-                    seed=config['seed'],
-                    styles=[],
-                    cfg_scale=7,
-                    sampler_index=config['sampler'],
-                    steps=config['steps'],
+  prompt = "%s%s%s" % (options.prompt, ', ' if options.styles else '', (options.styles))
+
+  res = api.txt2img(prompt=prompt,
+                    negative_prompt=options.negative_prompt,
+                    width=options.width,
+                    height=options.height,
+                    seed=options.seed,
+                    styles=[], # concatenated with prompt, doesn't get applied here for some reason
+                    cfg_scale=options.cfg,
+                    sampler_index=options.sampler,
+                    steps=options.steps,
                    )
-  
-  return (config, res)
 
-def save_text_properties(config, opts, output_file):
+  return (prompt, res)
+
+def save_text_properties(options, output_file):
   txtfile = open(output_file, 'w')
-  text = [config['prompt'], "\n", "Steps: %d, Sampler: %s, CFG scale: %d, Seed: %d" %
-        (config['steps'], config['sampler'], config['cfg'], config['seed'])]
+  text = [options.prompt, "\n", "Steps: %d, Sampler: %s, CFG scale: %d, Seed: %d, Size: %dx%d, Model: %s" %
+        (options.steps, options.sampler, options.cfg, options.seed, options.width, options.height, options.model)]
+  
   txtfile.writelines(text)
   txtfile.close()
-  if opts.showinfo:
+  if options.showinfo:
     print(''.join(text))
-
-
-def generate(opts):
-  
-  def _generate(opts):
-    prompt = opts.prompt
-
-    config, res = generate_image(opts)
-
-    output_file = ('%d_%s' % (config['seed'], prompt))
-    image_name = sanitize_filename_part(output_file)
-    res.image.save(image_name + '.png')
-    save_text_properties(config, opts, image_name + '.txt')
-
-    # if making a batch, create a collage of all images
-    if opts.showimg:
-      image=Image.open(image_name + '.png')
-      image.show()
-  
-  if int(opts.batch) > 1:
-    for x in range(0, int(opts.batch)):
-      _generate(opts)
-  else:
-      _generate(opts)
 
 def prettyprint(var):
   import pprint
@@ -140,8 +166,62 @@ def prettyprint(var):
   pp.pprint(var)
   sys.exit()
 
+def read_csv(options):
+  import csv
+
+  with open(options.promptcsv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    
+    for row in csv_reader:
+      if line_count == 0:
+        line_count += 1
+      else:
+        options.prompt = row[0]
+        options.negative_prompt = row[1] if row[1] else ''
+        options.width = row[2] if row[2].isdigit() else defaults['width']
+        options.height = row[3] if row[3].isdigit() else defaults['height']
+        options.steps = row[4] if row[4].isdigit() else defaults['steps']
+        options.seed = row[5] if row[5].isdigit() else defaults['seed']
+        options.cfg = row[6] if row[6].isdigit() else defaults['cfg']
+        options.sampler = row[7] if row[7] else defaults['sampler']
+        options.model = row[8] if row[8] else defaults['model']
+        options.styles = row[9] if row[9] else defaults['styles']
+        
+        line_count += 1
+        current_model = api.util_get_current_model()
+        
+        if options.model != current_model:
+          api.util_set_model(options.model)
+        
+        generate(options)
+  sys.exit()
+
+def generate(options):
+
+  def _generate(options):
+    (prompt, res) = generate_image(options)
+
+    output_file = ('%d_%s' % (options.seed, prompt))
+    image_name = options.output_dir + '\\' + sanitize_filename_part(output_file)
+    res.image.save(image_name + '.png')
+    if options.saveinfo:
+      save_text_properties(options, image_name + '.txt')
+
+    if options.showimg:
+      image=Image.open(image_name + '.png')
+      image.show()
+  
+  if int(options.batch) > 1:
+    for x in range(0, int(options.batch)):
+      _generate(options)
+  else:
+      _generate(options)
+
 if __name__ == '__main__':
-  opts = parse_args()
+  read_yaml()
+  (opts, usage) = parse_args()
+
   # create API client with custom host, port
   api = webuiapi.WebUIApi(host=opts.host, port=opts.port)
   
@@ -150,8 +230,8 @@ if __name__ == '__main__':
     print('Current model: %s' % model)
     sys.exit()
 
-  if opts.setmodel:
-    api.util_set_model(opts.setmodel)
+  if opts.model:
+    api.util_set_model(opts.model)
     
   if opts.listmodels:
     models = api.util_get_model_names()
@@ -173,11 +253,20 @@ if __name__ == '__main__':
     options = api.get_options() 
     prettyprint(options)
 
+  if opts.promptcsv:
+    read_csv(opts)
+
   if opts.prompts:
     with open(opts.prompts) as prompts:
       for line in prompts:
         opts.prompt = line
-        print(opts.prompt)
         generate(opts)
-  else:
+  
+  elif opts.prompt != None:
       generate(opts)
+  
+  #elif len(sys.argv) > 2:
+  #  opts.prompt = sys.argv[1]
+  
+  else:
+    usage()
