a69739a09823910229092370dfe4679e8a534102
terbo <terry.chad@gmail.com>
2023-01-20 19:15:37 -0600

    added comment regarding updating pip

--- update.bat
+++ update.bat
@@ -1,5 +1,8 @@
 rem place in webui directory
 rem if pip produces errors in red text, re-run this script
+rem additionally you may need to update pip, you can do that
+rem by running the following line before this script (without the "rem" part):
+rem pip install --upgrade pip
 
 call venv\scripts\activate.bat
 git pull
