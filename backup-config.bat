ad9cad457b621d726fc1d54945c0d6d3dabac39f
terbo <terry.chad@gmail.com>
2023-01-23 06:26:58 -0600

    added pause at the end

--- backup-config.bat
+++ backup-config.bat
@@ -1,4 +1,8 @@
 rem place in automatic 1111 webui direcory and run to backup main files
+rem you must manually backup your output\ directory.
+
 mkdir backup
 for %%a in (embeddings\*) do xcopy "%%a" "backup\embeddings\"
 for %%a in (config.json ui-config.json styles.csv webui-user.bat webui-user.sh webui-macos-env.sh) do xcopy "%%a" "backup"
+
+pause
